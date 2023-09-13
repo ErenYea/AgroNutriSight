9145ccaa690ea40ef93bc37e0d6652e3cc1f47dd
DrStone <drstoneluffy@gmail.com>
2023-09-13 22:30:47 +0500

    Initialized the project

--- src/blocks/menu/HomeHeaderMenuPrimary.js
+++ src/blocks/menu/HomeHeaderMenuPrimary.js
@@ -1,122 +1,170 @@
-import React, { Component } from 'react';
-import { Link } from 'react-scroll';
+import React, { Component } from "react";
+import { Link } from "react-scroll";
 
 class HomeHeaderMenuPrimary extends Component {
-    constructor() {
-        super();
-        this.state = {
-            over: false,
-            pushed: false
-        };
-
-        this.scrollEvent     = this.scrollEvent.bind( this );
-        this.set             = this.set.bind( this );
+  constructor() {
+    super();
+    this.state = {
+      over: false,
+      pushed: false,
     };
 
-    componentDidMount() {
-        window.addEventListener( 'scroll', this.scrollEvent, { passive: true } );
-    };
+    this.scrollEvent = this.scrollEvent.bind(this);
+    this.set = this.set.bind(this);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener( 'scroll', this.scrollEvent, { passive: true } );
-    };
+  componentDidMount() {
+    window.addEventListener("scroll", this.scrollEvent, { passive: true });
+  }
 
-    removeActive() {
-        if ( document.querySelector( '.active' ) ) {
-            let actives = document.querySelector( '.active' );
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.scrollEvent, { passive: true });
+  }
 
-            if ( actives !== null ) {
-                actives.classList.remove( "active" );
-            }
-        }
-    };
+  removeActive() {
+    if (document.querySelector(".active")) {
+      let actives = document.querySelector(".active");
 
-    scrollEvent() {
-        let offset = 76;
-        const offsets = [];
-        const link_names = [];
-        
-        offsets.push( document.getElementById( "services" ).offsetTop );
-        offsets.push( document.getElementById( "about-us" ).offsetTop );
-        offsets.push( document.getElementById( "team-members" ).offsetTop );
-        offsets.push( document.getElementById( "price-plans" ).offsetTop );
-        offsets.push( document.getElementById( "testimonials" ).offsetTop );
-        offsets.push( document.getElementById( "news" ).offsetTop );
-        offsets.push( document.getElementById( "contacts" ).offsetTop );
+      if (actives !== null) {
+        actives.classList.remove("active");
+      }
+    }
+  }
 
-        link_names.push( document.getElementById( "nav-services" ) );
-        link_names.push( document.getElementById( "nav-about" ) );
-        link_names.push( document.getElementById( "nav-teams" ) );
-        link_names.push( document.getElementById( "nav-pricing" ) );
-        link_names.push( document.getElementById( "nav-testimonials" ) );
-        link_names.push( document.getElementById( "nav-news" ) );
-        link_names.push( document.getElementById( "nav-contacts" ) );
+  scrollEvent() {
+    let offset = 76;
+    const offsets = [];
+    const link_names = [];
 
-        if ( !this.state.pushed ) {
-            if ( window.scrollY < offsets[0] ) {
-                this.removeActive();
-            }
+    offsets.push(document.getElementById("services").offsetTop);
+    offsets.push(document.getElementById("about-us").offsetTop);
+    offsets.push(document.getElementById("team-members").offsetTop);
+    offsets.push(document.getElementById("price-plans").offsetTop);
+    offsets.push(document.getElementById("testimonials").offsetTop);
+    offsets.push(document.getElementById("news").offsetTop);
+    offsets.push(document.getElementById("contacts").offsetTop);
 
-            for (let i = 0; i < offsets.length; i++) {
-                if ( window.scrollY + offset > offsets[i] ) {
-                    this.removeActive();
-                    link_names[i].classList.add( 'active' );
-                }  
-            }
-        }
-    };
+    link_names.push(document.getElementById("nav-services"));
+    link_names.push(document.getElementById("nav-about"));
+    link_names.push(document.getElementById("nav-teams"));
+    link_names.push(document.getElementById("nav-pricing"));
+    link_names.push(document.getElementById("nav-testimonials"));
+    link_names.push(document.getElementById("nav-news"));
+    link_names.push(document.getElementById("nav-contacts"));
 
-    handleClick( e ) {
-        this.setState( { pushed: true } )
+    if (!this.state.pushed) {
+      if (window.scrollY < offsets[0]) {
         this.removeActive();
+      }
 
-        let active = e.target;
-        active.parentNode.classList.add( "active" );
+      for (let i = 0; i < offsets.length; i++) {
+        if (window.scrollY + offset > offsets[i]) {
+          this.removeActive();
+          link_names[i].classList.add("active");
+        }
+      }
+    }
+  }
 
-        setTimeout( this.set, 800 );
-    };
+  handleClick(e) {
+    this.setState({ pushed: true });
+    this.removeActive();
 
-    set() {
-        this.setState( { pushed: false } )
-    };
+    let active = e.target;
+    active.parentNode.classList.add("active");
 
-    render() {
-        return (
-            <div className="menu">
-                <nav className="menu-primary">
-                    <ul className="nav">
-                        <li id="nav-services" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="Services" to="services" href="#services">Services</Link>
-                        </li>
+    setTimeout(this.set, 800);
+  }
 
-                        <li id="nav-about" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="About Us" to="about-us" href="#about-us">About Us</Link>
-                        </li>
+  set() {
+    this.setState({ pushed: false });
+  }
 
-                        <li id="nav-teams" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="Teams" to="team-members" href="#team-members">Team</Link>
-                        </li>
+  render() {
+    return (
+      <div className="menu">
+        <nav className="menu-primary">
+          <ul className="nav">
+            <li id="nav-services" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="Services"
+                to="services"
+                href="#services"
+              >
+                Products
+              </Link>
+            </li>
 
-                        <li id="nav-pricing" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="Pricing" to="price-plans" href="#price-plans">Pricing</Link>
-                        </li>
+            <li id="nav-teams" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="Teams"
+                to="team-members"
+                href="#team-members"
+              >
+                What we offer
+              </Link>
+            </li>
+            {/* 
+            <li id="nav-pricing" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="Pricing"
+                to="price-plans"
+                href="#price-plans"
+              >
+                Pricing
+              </Link>
+            </li> */}
 
-                        <li id="nav-testimonials" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="Testimonials" to="testimonials" href="#testimonials">Testimonials</Link>
-                        </li>
+            <li id="nav-testimonials" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="Testimonials"
+                to="testimonials"
+                href="#testimonials"
+              >
+                Health Tech
+              </Link>
+            </li>
 
-                        <li id="nav-news" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="News" to="news" href="#news">News</Link>
-                        </li>
+            <li id="nav-news" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="News"
+                to="news"
+                href="#news"
+              >
+                News
+              </Link>
+            </li>
+            <li id="nav-about" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="About Us"
+                to="about-us"
+                href="#about-us"
+              >
+                About Us
+              </Link>
+            </li>
 
-                        <li id="nav-contacts" className="nav-item">
-                            <Link onClick={ ( e ) => this.handleClick( e ) } title="Contacts" to="contacts" href="#contacts">Contacts</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        );
-    };
-};
+            <li id="nav-contacts" className="nav-item">
+              <Link
+                onClick={(e) => this.handleClick(e)}
+                title="Contacts"
+                to="contacts"
+                href="#contacts"
+              >
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default HomeHeaderMenuPrimary;
--- src/blocks/page-title/PageTitleHome.js
+++ src/blocks/page-title/PageTitleHome.js
@@ -1,41 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import ModalMenuLanguages from '../menu/ModalMenuLanguages';
+import ModalMenuLanguages from "../menu/ModalMenuLanguages";
 
 const PageTitleHome = () => {
-    return (
-        <section id="page-title" className="block with-img with-service-items" style={ { backgroundImage: `url( ${ require( "../../assets/img/placeholder/1920x1000.jpg" ) } )` } }>
-            <div className="wrapper d-flex flex-column justify-content-center">
-                <div className="page-title-body page-title-body-space-left">
-                    <div className="title">
-                        <h1 className="large">Business<br/> Consulting Agency</h1>
-                    </div>
+  return (
+    <section
+      id="page-title"
+      className="block with-img with-service-items"
+      style={{
+        backgroundImage: `url( ${require("../../assets/img/placeholder/1920x1000.jpg")} )`,
+      }}
+    >
+      <div className="wrapper d-flex flex-column justify-content-center">
+        <div className="page-title-body page-title-body-space-left">
+          <div className="title">
+            <h1 className="large">
+              Empowering Precision
+              <br /> through Artificial Intelligence
+            </h1>
+          </div>
 
-                    <div className="description spacer p-top-lg">
-                        <p>We are digital business consulting agency that helps companies increase better incomes and help with branding for faster business growing.</p>
-                    </div>
+          <div className="description spacer p-top-lg">
+            <p>
+              At Biologic AI, we are at the forefront of a radiological
+              revolution, driven by the transformative potential of Artificial
+              Intelligence. With unwavering dedication and a commitment to
+              excellence, we are reshaping the landscape of medical imaging to
+              provide cutting-edge solutions that empower healthcare
+              professionals and enhance patient care
+            </p>
+          </div>
 
-                    <div className="spacer p-top-lg no-space">
-                        <a href={ process.env.PUBLIC_URL + "/about-us"} className="btn btn-outline-tertiary">Learn More</a>
-                    </div>
-                </div>
+          <div className="spacer p-top-lg no-space">
+            <a
+              href={process.env.PUBLIC_URL + "/about-us"}
+              className="btn btn-outline-tertiary"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
 
-                <div className="page-title-footer full-width">
-                    <div className="d-flex justify-content-between">
-                        <a href="#services" className="scroll-to-id scroll-animation">
-                            <i className="malex-icon-long-arrow-down i-large-2"></i>
-                        </a>
+        <div className="page-title-footer full-width">
+          <div className="d-flex justify-content-between">
+            <a href="#services" className="scroll-to-id scroll-animation">
+              <i className="malex-icon-long-arrow-down i-large-2"></i>
+            </a>
 
-                        <div className="align-self-end">
+            {/* <div className="align-self-end">
                             <ModalMenuLanguages />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </div> */}
+          </div>
+        </div>
+      </div>
 
-            <div className="page-title-bg-color"></div>
-        </section>
-    );
+      <div className="page-title-bg-color"></div>
+    </section>
+  );
 };
 
 export default PageTitleHome;
