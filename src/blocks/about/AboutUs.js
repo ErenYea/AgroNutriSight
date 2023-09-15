212e9a693264876bfaea3f1b72c90cd872e02ec3
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-16 03:39:22 +0500

    horizontal animation added

--- src/blocks/about/AboutUs.js
+++ src/blocks/about/AboutUs.js
@@ -1,7 +1,29 @@
 import React from "react";
-import "./about.css"; 
+import "./about.css";
 
-const AboutsUs = () => {
+const AboutUs = () => {
+
+  const itemsToRepeat = [
+    "Innovating Healthcare Horizons",
+    "Customer-Centric Excellence",
+    "Personalized Healthcare",
+    "AI-Powered Drug Discovery",
+    "Global Health Impact",
+    "Health & Wellness Tips",
+    "Chronic Disease Management"
+  ];
+
+  const repeatedItems = Array.from({ length: 1000 }).map((_, index) => (
+    <div className="icon-list-item" key={index}>
+      <p>
+        <span className="d-flex">
+          <i className="fas fa-check"></i>
+          <span>{itemsToRepeat[index % itemsToRepeat.length]}</span>
+        </span>
+      </p>
+    </div>
+  ));
+
   return (
     <div className="wrapper" style={{ marginTop: "4rem" }}>
       <div className="title">
@@ -11,60 +33,29 @@ const AboutsUs = () => {
       <div className="row gutter-width-md with-pb-md">
         <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
           <div className="description-2">
-            <h2 className="text-justify">Biologic AI - Where Science Meets Compassion in Pursuit of a Healthier Tomorrow</h2>
+            <h2 className="text-justify">
+              Biologic AI - Where Science Meets Compassion in Pursuit of a Healthier Tomorrow
+            </h2>
           </div>
         </div>
 
         <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
           <div className="description-2">
             <p className="text-justify">
-              Our unwavering mission is to transform healthcare by unlocking the limitless capabilities of AI-driven solutions. We firmly believe that cutting-edge technology has the potential to tackle the most intricate challenges in healthcare, spanning from early disease detection to the precision of treatment planning.
+              Our unwavering mission is to transform healthcare by unlocking the limitless capabilities
+              of AI-driven solutions. We firmly believe that cutting-edge technology has the potential
+              to tackle the most intricate challenges in healthcare, spanning from early disease
+              detection to the precision of treatment planning.
             </p>
           </div>
         </div>
       </div>
 
       <div className="icon-list">
-        <div className="icon-list-items">
-          <div className="icon-list-item">
-            <p>
-              <span className="d-flex">
-                <i className="fas fa-check"></i>
-                <span> Innovating Healthcare Horizons </span>
-              </span>
-            </p>
-          </div>
-
-          <div className="icon-list-item">
-            <p>
-              <span className="d-flex">
-                <i className="fas fa-check"></i>
-                <span> Customer-Centric Excellence </span>
-              </span>
-            </p>
-          </div>
-
-          <div className="icon-list-item">
-            <p>
-              <span className="d-flex">
-                <i className="fas fa-check"></i>
-                <span> Unparalleled Quality Assurance </span>
-              </span>
-            </p>
-          </div>
-
-          <div className="icon-list-item">
-            <p>
-              <span className="d-flex">
-                <i className="fas fa-check"></i>
-                <span> Our Satisfaction Guarantee </span>
-              </span>
-            </p>
-          </div>
-        </div>
+        <div className="icon-list-items">{repeatedItems}</div>
       </div>
     </div>
   );
 };
 
-export default AboutsUs;
+export default AboutUs;
