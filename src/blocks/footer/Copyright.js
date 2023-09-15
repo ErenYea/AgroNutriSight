1070c6db1a7221bf7a83425ee08eb5444a0745a6
DrStone <drstoneluffy@gmail.com>
2023-09-15 21:14:15 +0500

    Work on services

--- src/blocks/footer/Copyright.js
+++ src/blocks/footer/Copyright.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Copyright = () => {
-    return (
-        <div className="copyright">
-            <p>© { new Date().getFullYear() } Business Consulting Agency React JS Template By <a href="https://www.adveits.com">Adveits</a></p>
-        </div>
-    );
+  return (
+    <div className="copyright">
+      <p>
+        © {new Date().getFullYear()} Empowering Precision through AI By{" "}
+        <a href="https://www.adveits.com">Biological Ai</a>
+      </p>
+    </div>
+  );
 };
 
 export default Copyright;
